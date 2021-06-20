258d63d009a0c30a7c3d878616950038e8b33af3
echenim <myron.echenim@gmail.com>
2021-06-20 08:15:46 -0700

    log defination and implementation

---
+++ log.go
@@ -0,0 +1,160 @@
+package journal
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	d "github.com/echenim/journal/declarations"
+	l "github.com/echenim/journal/plugs"
+)
+
+var std = l.New(os.Stderr, "", d.LstdFlags, d.Debug)
+
+// SetOutput sets the output destination for the standard logger
+func SetOutput(out io.Writer) {
+	std = l.New(out, std.Prefix(), std.Flags(), std.Precedency)
+}
+
+// Priority returns the output priority for the standard logger.
+func Priority() int {
+	return std.Priority()
+}
+
+// SetPriority sets the output priority for the standard logger.
+func SetPriority(precedency int) {
+	std.SetPriority(precedency)
+}
+
+// SetPriorityString sets the output priority by the name of a debug level
+func SetPriorityString(s string) error {
+	s = strings.ToUpper(s)
+	for i, name := range d.PrecencyName {
+		if name == s {
+			SetPriority(i)
+			return nil
+		}
+	}
+	return fmt.Errorf("Unable to find priority %s", s)
+}
+
+// Flags returns the output layouts for the standard logger.
+func Flags() int {
+	return std.Flags()
+}
+
+// SetFlags sets the output layouts for the standard logger.
+func SetFlags(flags int) {
+	std.SetFlags(flags)
+}
+
+// SetPrefix sets the logger prefix
+func SetPrefix(prefix string) {
+	std.SetPrefix(prefix)
+}
+
+// Fatal prints the message it's given and quits the program
+func Fatal(v ...interface{}) {
+	std.Fatal(v...)
+}
+
+// Fatalf prints the message it's given and quits the program
+func Fatalf(format string, v ...interface{}) {
+	std.Fatalf(format, v...)
+}
+
+// Fatalln prints the message it's given and quits the program
+func Fatalln(v ...interface{}) {
+	std.Fatalln(v...)
+}
+
+// Panic prints the message it's given and panic()s the program
+func Panic(v ...interface{}) {
+	std.Panic(v...)
+}
+
+// Panicf prints the message it's given and panic()s the program
+func Panicf(format string, v ...interface{}) {
+	std.Panicf(format, v...)
+}
+
+// Panicln prints the message it's given and panic()s the program
+func Panicln(v ...interface{}) {
+	std.Panicln(v...)
+}
+
+// Error prints to the standard logger with the Error level.
+func Error(v ...interface{}) {
+	std.Error(v...)
+}
+
+// Errorf prints to the standard logger with the Error level.
+func Errorf(format string, v ...interface{}) {
+	std.Errorf(format, v...)
+}
+
+// Errorln prints to the standard logger with the Error level.
+func Errorln(v ...interface{}) {
+	std.Errorln(v...)
+}
+
+// Warn prints to the standard logger with the Warn level.
+func Warn(v ...interface{}) {
+	std.Warn(v...)
+}
+
+// Warnf prints to the standard logger with the Warn level.
+func Warnf(format string, v ...interface{}) {
+	std.Warnf(format, v...)
+}
+
+// Warnln prints to the standard logger with the Warn level.
+func Warnln(v ...interface{}) {
+	std.Warnln(v...)
+}
+
+// Info prints to the standard logger with the Info level.
+func Info(v ...interface{}) {
+	std.Info(v...)
+}
+
+// Infof prints to the standard logger with the Info level.
+func Infof(format string, v ...interface{}) {
+	std.Infof(format, v...)
+}
+
+// Infoln prints to the standard logger with the Info level.
+func Infoln(v ...interface{}) {
+	std.Infoln(v...)
+}
+
+// Debug prints to the standard logger with the Debug level.
+func Debug(v ...interface{}) {
+	std.Debug(v...)
+}
+
+// Debugf prints to the standard logger with the Debug level.
+func Debugf(format string, v ...interface{}) {
+	std.Debugf(format, v...)
+}
+
+// Debugln prints to the standard logger with the Debug level.
+func Debugln(v ...interface{}) {
+	std.Debugln(v...)
+}
+
+// Trace prints to the standard logger with the Trace level.
+func Trace(v ...interface{}) {
+	std.Trace(v...)
+}
+
+// Tracef prints to the standard logger with the Trace level.
+func Tracef(format string, v ...interface{}) {
+	std.Tracef(format, v...)
+}
+
+// Traceln prints to the standard logger with the Trace level.
+func Traceln(v ...interface{}) {
+	std.Traceln(v...)
+}
